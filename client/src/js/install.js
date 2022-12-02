e25ff96387c60ba5e96c460365e6cc4305f3fb83
Beth DeCarlo <110702442+bethdecarlo@users.noreply.github.com>
2022-12-01 21:40:11 -0500

    added dist folder to client directory

---
+++ client/src/js/install.js
@@ -0,0 +1,32 @@
+const butInstall = document.getElementById("buttonInstall");
+
+window.addEventListener('beforeinstallprompt', (event) => {
+
+    // Store triggered events
+    window.deferredPrompt = event;
+
+    // Remove hidden class from button.
+    butInstall.classList.toggle('hidden', false);
+  });
+
+butInstall.addEventListener('click', async () => {
+  
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset deferred prompt variable
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 
